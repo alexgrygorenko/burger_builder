99c1a1ddb8f7af8fe6d0d3d04cc80af45552c336
Alex Grygorenko <alexey.grygorenko@gmail.com>
2019-07-06 08:13:29 +0300

    first and last commit

---
+++ src/store/reducers/auth.test.js
@@ -0,0 +1,13 @@
+import reducer from "./auth";
+
+describe("auth reducer", () => {
+  it("shoul return the initial state", () => {
+    expect(reducer(undefined, {})).toEqual({
+      token: null,
+      userId: null,
+      error: null,
+      loading: false,
+      authRedirectPath: "/"
+    });
+  });
+});
